bb582b0cf85de747f1f255a68b07dd7d16eacdb1
Anders Vestergaard <a.vestergaard@uber.com>
2023-04-29 14:10:50 +0200

    ssr

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,17 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: ["gatsby-plugin-styled-components", {
+    resolve: "gatsby-plugin-styletron",
+    options: {
+      // You can pass options to Styletron.
+      // Prefix all generated classNames:
+      prefix: "_",
+    },
+  },],
+  flags: {
+    DEV_SSR: true
+  },
 };
 
 export default config;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,193 +1,182 @@
-import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
+import React, { Dispatch, SetStateAction } from "react";
+import { Provider as StyletronProvider } from "styletron-react";
+import { LightTheme, BaseProvider, styled } from "baseui";
+import * as ButtonGroup from "baseui/button-group";
+import * as Button from "baseui/button";
+import * as FormControl from "baseui/form-control";
+import * as Accordion from "baseui/accordion";
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+const isBrowser = typeof window !== "undefined";
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
+export default function Index() {
+  const [engine, setEngine] = React.useState(null);
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
+  const [selected, setSelected] = useLocalStorage({
+    key: "bet-0",
+    defaultValue: defaultBet,
+  });
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
+  React.useEffect(() => {
+    // Load the `styletron-engine-atomic` package dynamically.
+    // Reason: It requires use of `document`, which is not available
+    // outside the browser, so we need to wait until it successfully loads.
+    // Source: https://www.gatsbyjs.org/docs/debugging-html-builds/
+    import("styletron-engine-atomic").then((styletron) => {
+      const clientEngine = new styletron.Client();
+      setEngine(clientEngine);
+    });
+  }, []);
 
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
+  if (!engine) return null;
+
+  const result = prepareResult(selected);
+
+  function handleClick(i: number, value: string) {
+    const stringArr = Array.from(selected);
+    stringArr[i] = value;
+    setSelected(stringArr.join(""));
   }
-]
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-const IndexPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <Wrapper>
+          <div>
+            {matches.map((match, i) => {
+              return (
+                <FormControl.FormControl
+                  key={i}
+                  label={`${i + 1}: ${match.label}`}
+                >
+                  <ButtonGroup.ButtonGroup>
+                    <Button.Button
+                      isSelected={selected[i] === "1"}
+                      onClick={() => handleClick(i, "1")}
+                    >
+                      1
+                    </Button.Button>
+                    <Button.Button
+                      isSelected={selected[i] === "X"}
+                      onClick={() => handleClick(i, "X")}
+                    >
+                      x
+                    </Button.Button>
+                    <Button.Button
+                      isSelected={selected[i] === "2"}
+                      onClick={() => handleClick(i, "2")}
+                    >
+                      2
+                    </Button.Button>
+                  </ButtonGroup.ButtonGroup>
+                </FormControl.FormControl>
+              );
+            })}
+          </div>
+        </Wrapper>
+      </BaseProvider>
+    </StyletronProvider>
+  );
 }
 
-export default IndexPage
+function formatLinenumbers(lines: number[]) {
+  let res = lines?.slice(0, Math.min(lines.length, 10))?.join(",") ?? "";
+  if (lines?.length > 10) {
+    res += "...";
+  }
+  return res;
+}
 
-export const Head: HeadFC = () => <title>Home Page</title>
+function prepareResult(currentStatus: string) {
+  const result: { [k: number]: number[] } = {};
+
+  tips.forEach((bets, betsIndex) => {
+    let correct = 0;
+    for (let i = 0; i < bets.length; i++) {
+      const bet = bets[i];
+      const curr = currentStatus[i];
+      if (bet === curr) {
+        correct++;
+      }
+    }
+    let entry = result[correct];
+    if (entry == null) {
+      entry = [];
+    }
+    entry.push(betsIndex);
+    result[correct] = entry;
+  });
+  return result;
+}
+
+const matches = [
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+];
+
+const copy =
+  "11112111X21212.111121X21112X3.111212XX2111X4.11X112X11211X5.11XXX21X12X2X6.11X221X11X2217.11211XX11X11X8.11211211221219.1121X11112X2210.112121112212111.112121211112112.11212X121212113.11212211X212214.112122X1112X115.112XX1111212116.112XXX1X1212117.11221X111112118.1X11111X1222119.1X21211X212X220.1X2XX1111222121.1X2XX1211X1X122.1X2X2X111X12123.1211X21X1112124.121X121112X2X25.12X1X2121122126.1221111X1212127.12211X111212128.1221X11X1112129.1221221122X2130.122X2111112X131.1222X2111112232.X1111X11112X133.X111X1X112X1134.X11XX111XXX2135.X1121X1X1111X36.X1X1XX112212137.X1X1XXX22X12138.X1XXXX121212139.X1X221X11122140.X12112111211141.X121X1222111X42.X12121112XXX143.X12121X11112244.X12121X112X2145.X12121211212146.X1212211111XX47.XX11122212X1148.XXXX22111211X49.XX2122111212250.XX2122X21211151.X2X1XXXX11X1152.X2X12XX122X1X53.X2X122XX1212154.211X1XX11X12155.211XXX212X1X156.211XX21X111X157.211X222X12XX158.21X1111X1122X59.21X1XX1X1121160.21X12X1112X2261.21X122X11212162.21XXX2X1X112163.21XX22211X11164.21X22XX11212165.2121XX11X112166.212121121111167.212121X11212268.2121221X1112269.212X22111X22170.2XX12X111121171.2X211211X2X2272.2X2121111X12173.2X212112X112174.221122XX1211175.22X11X1112221";
+
+const tips = copy
+  .toUpperCase()
+  .split(".")
+  .map((tip) => tip.slice(0, 13));
+const defaultBet = "1111111111111";
+
+function useLocalStorage({
+  key,
+  defaultValue,
+}: {
+  key: string;
+  defaultValue: string;
+}): [string, Dispatch<SetStateAction<string>>] {
+  const [localStorage, setLocalStorage] = React.useState(() => {
+    return getLocalStorageObject({ key, defaultValue });
+  });
+
+  React.useEffect(() => {
+    if (localStorage) {
+      window.localStorage.setItem(key, localStorage);
+    }
+  }, [localStorage, key]);
+
+  return [localStorage, setLocalStorage];
+}
+
+function getLocalStorageObject({
+  key,
+  defaultValue,
+}: {
+  key: string;
+  defaultValue: string;
+}) {
+  let localStorageJson = null;
+  if (isBrowser) {
+    localStorageJson = window.localStorage.getItem(key);
+  }
+  return localStorageJson ? localStorageJson : defaultValue;
+}
+
+const Wrapper = styled("div", {
+  display: "flex",
+  gap: "20px",
+});
+
+const ResultWrapper = styled("div", {
+  minWidth: "500px",
+});
+
+const CorrectBet = styled("span", ({ $theme }) => ({
+  minWidth: "500px",
+  color: $theme.colors.positive,
+}));
